a6fbffb28b4dd5fe37c9de57c1c7757647744be3
Adubiagbe Mustapha <jidemustapha2013@gmail.com>
2019-06-09 13:58:57 -0500

    Apply fixes from StyleCI (#7)

--- src/ChuckNorrisJokesServiceProvider.php
+++ src/ChuckNorrisJokesServiceProvider.php
@@ -17,19 +17,19 @@ class ChuckNorrisJokesServiceProvider extends ServiceProvider
             ]);
         }
 
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'chuck-norris');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'chuck-norris');
 
         $this->publishes([
-            __DIR__ . '/../resources/views' => resource_path('views/vendor/chuck-norris')
+            __DIR__.'/../resources/views' => resource_path('views/vendor/chuck-norris'),
         ], 'views');
 
         $this->publishes([
-            __DIR__ . '/../config/chuck-norris.php' => base_path('config/chuck-norris.php')
+            __DIR__.'/../config/chuck-norris.php' => base_path('config/chuck-norris.php'),
         ], 'config');
 
         if (! class_exists('CreateJokesTable')) {
             $this->publishes([
-                __DIR__ . '../database/migrations/create_jokes_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_jokes_table.php')
+                __DIR__.'../database/migrations/create_jokes_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_jokes_table.php'),
             ], 'migrations');
         }
 
